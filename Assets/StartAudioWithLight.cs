5d89a235882668beb1f5d13b332ef92decf62966
zaza303 <zazik2468013579@mail.ru>
2022-12-05 11:37:33 +0800

    Button on main desk is added

---
+++ Assets/StartAudioWithLight.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+
+public class StartAudioWithLight : MonoBehaviour
+{
+    public GameObject SpotLight;
+    public GameObject Sound;
+    private Light light;
+    private AudioSource audio;
+    // Start is called before the first frame update
+    void Start()
+    {
+        light = SpotLight.GetComponent<Light>();
+        audio = Sound.GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    public void BClick()
+    {
+        if (!audio.isPlaying) audio.Play();
+        else audio.Stop();
+
+
+    }
+    public void Update()
+    {
+        light.intensity = audio.isPlaying ? 0 : 2;
+    }
+}
