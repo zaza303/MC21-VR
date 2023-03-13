f2d412a9bf32271a8468f40c290daa36a7a5c0ad
Newddr <fedtur62@yandex.ru>
2023-03-13 16:02:40 +0300

    Scripts
    Скрипты для генерации деталей и взаимодействия с ними инструментов

---
+++ Assets/Scripts/Exam/createDetail.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class createDetail : MonoBehaviour
+{
+    [SerializeField] private Vector3 spawnPoint;
+    [SerializeField] private GameObject[] tools;
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+       GameObject objectToSpawn =  SelectItem();
+       CreateObject(objectToSpawn);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    GameObject SelectItem()
+    {
+        
+        var index = Random.Range(0,tools.Length);
+        return tools[index];
+    }
+    void CreateObject(GameObject objectToSpawn)
+    {
+
+        Instantiate(objectToSpawn,spawnPoint,Quaternion.identity);
+    }
+}
