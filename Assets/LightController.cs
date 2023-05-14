40cf6348c329b5a5f990ffe922bb25d512b8d618
TetsuyaTobiichi <revenger.crut@mail.ru>
2023-05-14 19:07:50 +0800

    добавленны аудио подсказки

---
+++ Assets/LightController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightController : MonoBehaviour
+{
+    private Light lg;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("startedButton");
+        
+    }
+
+    // Update is called once per frame
+    public void BClick()
+    {
+        Debug.Log("worked");
+        
+    }
+}
---
+++ Assets/promt.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class promt : MonoBehaviour
+{
+
+    private VRTK_InteractableObject interactableObject;
+    private AudioSource audioSource;
+    private bool played = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("123");
+
+        interactableObject = GetComponent<VRTK_InteractableObject>();
+        audioSource = GetComponent<AudioSource>();
+
+        interactableObject.InteractableObjectGrabbed += OnObjectGrable;
+    }
+
+  
+    private void OnObjectGrable(object sender, InteractableObjectEventArgs e)
+    {
+
+        Debug.Log("grabbed");
+        if (!played)
+        {
+            audioSource.Play();
+            played = true;
+        }
+    }
+}
